efc4a461fb6817eed214a630c8dc36b9908a9820
Thiago Amorim <thiago.amorimsilva38@gmail.com>
2023-05-26 21:35:07 -0300

    exclui comentarios a respeito de autorias do codigo

--- App - WS3D/src/main/java/ExperimentMain.java
+++ App - WS3D/src/main/java/ExperimentMain.java
@@ -1,32 +1,7 @@
-/*****************************************************************************
- * Copyright 2007-2015 DCA-FEEC-UNICAMP
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *  http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * 
- * Contributors:
- *    Klaus Raizer, Andre Paraense, Ricardo Ribeiro Gudwin
- *****************************************************************************/
 
-import br.unicamp.cst.core.entities.Codelet;
-import br.unicamp.cst.util.MindViewer;
-import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-/**
- *
- * @author rgudwin
- */
 public class ExperimentMain {
         
         public Logger logger = Logger.getLogger(ExperimentMain.class.getName());
--- Simulador - WS3D/src/main/java/motorcontrol/TwoWheeledRobotKinematics.java
+++ Simulador - WS3D/src/main/java/motorcontrol/TwoWheeledRobotKinematics.java
@@ -1,37 +1,8 @@
-/*****************************************************************************
- * Copyright 2007-2015 DCA-FEEC-UNICAMP
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *  http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * 
- * Contributors:
- *    Patricia Rocha de Toro, Elisa Calhau de Castro, Ricardo Ribeiro Gudwin
- *****************************************************************************/
 package motorcontrol;
 
-import com.jme.util.export.InputCapsule;
-import com.jme.util.export.JMEExporter;
-import com.jme.util.export.JMEImporter;
-import com.jme.util.export.OutputCapsule;
-import com.jme.util.export.Savable;
-import java.io.IOException;
 import model.Creature;
-import model.Environment;
 import util.Constants;
 
-/**
- *
- * @author eccastro
- */
 public class TwoWheeledRobotKinematics implements CreatureKinematicsInterface {
 
     private Creature c;
@@ -47,11 +18,9 @@ public class TwoWheeledRobotKinematics implements CreatureKinematicsInterface {
     public synchronized void updatePosition() {
         // double D = 1.0 * 50.0;
         //double D = 1.0 * 3.0;
-        double D = 20 / Constants.M_PI;
         double pRad = Math.toRadians(c.getPitch());
         double aux = (1 - c.getFriction());
-        double w = (c.getVleft() - c.getVright()) / D; //clockwise is positive
-        double cosp, senp, a, cosWP, senWP;
+        double cosp, senp, a;
 
         cosp = Math.cos(-pRad); //counterclockwise is negative
         senp = Math.sin(-pRad);
